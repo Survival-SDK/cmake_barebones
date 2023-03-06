9fc4063e108a6f24d0d96bf96a953ba66bec2bee
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2023-03-06 23:18:42 +0700

    Add x86_64-linux-gnu-gcc toolchain file

---
+++ cmake/barebones/toolchains/x86_64-linux-gnu-gcc.cmake
@@ -0,0 +1,10 @@
+set(BB_TRIPLET x86_64-linux-gnu)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_C_COMPILER x86_64-linux-gnu-gcc)
+set(ENV{PKG_CONFIG_LIBDIR}
+    "/usr/x86_64-linux-gnu/lib/pkgconfig:/usr/local/x86_64-linux-gnu/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig")
+set(CMAKE_TRY_COMPILE_TARGET_TYPE EXECUTABLE)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
