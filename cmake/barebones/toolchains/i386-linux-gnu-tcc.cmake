4b300e0a7da65edb795ce053f0eaa17ffd0ba405
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2023-03-10 22:24:01 +0700

    Add very experimental (not seems to work) toolchains i386-linux-gnu-clang and i386-linux-gnu-tcc

---
+++ cmake/barebones/toolchains/i386-linux-gnu-tcc.cmake
@@ -0,0 +1,11 @@
+set(BB_TRIPLET i386-linux-gnu)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR i386)
+set(CMAKE_C_COMPILER i386-linux-gnu-tcc)
+set(CMAKE_C_FLAGS $ENV{CFLAGS} -Wunsupported)
+set(ENV{PKG_CONFIG_LIBDIR}
+    "/usr/i386-linux-gnu/lib/pkgconfig:/usr/local/i386-linux-gnu/lib/pkgconfig:/usr/lib/i386-linux-gnu/pkgconfig")
+set(CMAKE_TRY_COMPILE_TARGET_TYPE EXECUTABLE)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
