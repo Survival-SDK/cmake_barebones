5f54ae7201ea491d78fcc6eefd191f88c0349d62
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2023-03-11 21:47:29 +0700

    Change CMAKE_TRY_COMPILE_TARGET_TYPE for i386-linux-gnu-tcc

--- cmake/barebones/toolchains/i386-linux-gnu-tcc.cmake
+++ cmake/barebones/toolchains/i386-linux-gnu-tcc.cmake
@@ -5,7 +5,7 @@ set(CMAKE_C_COMPILER i386-linux-gnu-tcc)
 set(CMAKE_C_FLAGS $ENV{CFLAGS} -Wunsupported)
 set(ENV{PKG_CONFIG_LIBDIR}
     "/usr/i386-linux-gnu/lib/pkgconfig:/usr/local/i386-linux-gnu/lib/pkgconfig:/usr/lib/i386-linux-gnu/pkgconfig")
-set(CMAKE_TRY_COMPILE_TARGET_TYPE EXECUTABLE)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
