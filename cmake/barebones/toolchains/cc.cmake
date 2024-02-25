886b87d6f912c42267b76dbab8988e7f412bf9be
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2024-02-25 15:02:43 +0600

    Add toolchain file for host

---
+++ cmake/barebones/toolchains/cc.cmake
@@ -0,0 +1,10 @@
+set(CMAKE_C_COMPILER cc)
+set(CMAKE_CXX_COMPILER c++)
+set(CMAKE_FIND_ROOT_PATH
+    /usr
+    /usr/local
+)
+set(ENV{PKG_CONFIG_PATH}
+    "/usr/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig")
+set(ENV{PKG_CONFIG_LIBDIR}
+    "/usr/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig")
